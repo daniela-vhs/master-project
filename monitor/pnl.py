a417a5b243d54c2ce0f686907fee744b25fc6c60
Daniela Hidalgo <dani.hidalgo.s@gmail.com>
2026-08-10 11:45:57 +0100

    Model titles

--- monitor/pnl.py
+++ monitor/pnl.py
@@ -97,7 +97,7 @@ def pnl_tab(actual_pnl_df, sens_df):
 
             fig = go.Figure()
 
-            valid_values = {key: value for key, value in values.items() if value != 0}
+            valid_values = {key: value for key, value in values.items()}
 
             fig.add_trace(
                 go.Waterfall(
